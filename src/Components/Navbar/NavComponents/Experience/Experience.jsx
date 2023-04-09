d742b4face37cc4fb403ffd7cda7a8d15786e02c
ishanyadav13290 <ishanyadav13290@gmail.com>
2023-04-09 13:26:43 +0530

    new experience added

--- src/Components/Navbar/NavComponents/Experience/Experience.jsx
+++ src/Components/Navbar/NavComponents/Experience/Experience.jsx
@@ -1,11 +1,47 @@
 import { Box, Text } from "@chakra-ui/react";
+import TypewriterComponent from "typewriter-effect";
 
-export default function Experience(){
-    return <Box>
-        <Box>
-        <Text fontSize={'2xl'}>The Help Foundation</Text>
-        <Text as={"cite"} fontSize={"sm"}>Feb 2023 to March 2023</Text>
-        <Text as={"cite"} fontSize={"sm"}>Feb 2023 to March 2023</Text>
-        </Box>
+export default function Experience() {
+  return (
+    <Box>
+      <Box>
+        <Box fontWeight={700} overflow={"hidden"} fontSize={"4xl"}>
+<TypewriterComponent
+  onInit={(typewriter) => {
+    typewriter.start()
+    .typeString("My Experiences")
+    .pauseFor(1000)
+    .deleteChars(6)
+    .typeString(", a Full-Stack Developer (MERN)")
+  .pauseFor(1000)
+  .start()
+  }}
+  options={{
+    loop:true
+  }}
+/>
+</Box>
+      </Box>
+      <br />
+      <Box>
+        <Text fontSize={"2xl"} fontWeight={700}>
+          Indian Centre For Development & Rights (ICFDR)
+        </Text>
+        <Text as={"cite"} fontSize={"sm"}>
+          April 2023 to Present
+        </Text>
+      </Box>
+      <br />
+      <Box>
+        <Text fontSize={"2xl"} fontWeight={700}>The Help Foundation</Text>
+        <Text as={"cite"} fontSize={"sm"}>
+          Feb 2023 to March 2023
+        </Text>
+        <br />
+        <Text as={"cite"} fontSize={"sm"}>
+          Feb 2023 to March 2023
+        </Text>
+      </Box>
     </Box>
+  );
 }
