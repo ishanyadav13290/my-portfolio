668c7be59debed48813b143c161539d7a42a1218
ishanyadav13290 <ishanyadav13290@gmail.com>
2023-04-15 17:13:40 +0530

    daily push

--- src/Components/Navbar/NavComponents/Experience/Experience.jsx
+++ src/Components/Navbar/NavComponents/Experience/Experience.jsx
@@ -37,9 +37,6 @@ export default function Experience() {
         <Text as={"cite"} fontSize={"sm"}>
           Feb 2023 to March 2023
         </Text>
-        <Text as={"cite"} fontSize={"sm"}>
-          Feb 2023 to March 2023
-        </Text>
         
       </Box>
     </Box>
